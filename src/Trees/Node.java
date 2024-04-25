a0d1ea1753970930d8f4f5e1617e0d51ee0e0f09
Gustavo Ribeiro <gustavochiela@gmail.com>
2024-04-24 22:47:04 -0300

    Added the class Node

---
+++ src/Trees/Node.java
@@ -0,0 +1,14 @@
+package src.Trees;
+
+public class Node {
+    int value;
+    Node left, right;
+
+    public Node(int value) {
+        this.value = value;
+        left = right = null;
+    }
+
+    public Node(){}
+
+}
