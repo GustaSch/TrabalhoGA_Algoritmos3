d75625cf68673082694eced0d85e2cccf1b7ceb9
Yago Rodrigues <y.rodr1gues0811@gmail.com>
2024-04-25 16:13:01 -0300

    Added remove and post order method

--- src/Trees/Tree.java
+++ src/Trees/Tree.java
@@ -4,9 +4,7 @@ public class Tree {
     Node root;
 
     public Tree(int value) {
-        root = new Node();
-        root.value = value;
-        root.left = root.right = null;
+        root = new Node(value);
     }
 
     public Node getRoot() {
@@ -15,19 +13,75 @@ public class Tree {
 
     public void setRoot(Node root) {
         this.root = root;
-    }   
+    }
+
+    public Node insert(int value) {
+        return insert(value, root);
+    }
 
     private Node insert(int value, Node root) {
+        //base case
         if (root == null) {
-            root = new Node(value);
+            return new Node(value);
         }
 
-        if (value > root.value) {
-            insert(value, root.right);
+        if (value > root.getValue()) {
+            root.setRight(insert(value, root.getRight()));
+        } else {
+            root.setLeft(insert(value, root.getLeft()));
         }
-
-        insert(value, root.left);
 
         return root;
+    }
+
+    public Node remove(int value) {
+        return remove(root, value);
+    }
+
+    private Node remove(Node root, int value) {
+        if(root == null) return null;
+
+        else if (root.getValue() > value) root.setLeft(remove(root.getLeft(), value));
+
+        else if (root.getValue() < value) root.setRight(remove(root.getRight(), value));
+
+        else{ // Found the node to be removed
+            //Case 1: No child
+            if (root.getLeft() == null && root.getRight() == null) {
+                root = null;
+            }
+            //Case 2: One child
+            else if (root.getLeft() == null) {
+                root = root.getRight();
+            } else if (root.getRight() == null) {
+                root = root.getLeft();
+
+            } else { //Case 3: Two children
+                Node minimum = findMin(root.getRight());
+                root.setValue(minimum.getValue());
+                root.setRight(remove(root.getRight(), minimum.getValue()));
+            }
+        }
+        return root;
+    }
+
+    private Node findMin(Node right) {
+        while (right.getLeft() != null) {
+            right = right.getLeft();
+        }
+        return right;
+    }
+
+    public void postOrder() {
+        postOrder(root);
+        System.out.println();
+    }
+
+    private void  postOrder(Node root) {
+        if (root != null) {
+            postOrder(root.getLeft());
+            postOrder(root.getRight());
+            System.out.print(root.getValue() + " ");
+        }
     }
 }
